074df51fdb2b4290e1c546661f8484f3f7ab9f70
aacce <129754831+Omikate@users.noreply.github.com>
2026-09-01 11:04:36 +0700

    Coding ding ding dong

--- Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Enemy.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Enemy.cs
@@ -4,6 +4,17 @@ namespace Assignment.StudentSolution
 {
     public class Enemy
     {
+        public int damage;
+        protected int ailevel;
 
+        public virtual void Attack(Entity target)
+        {
+
+        }
+
+        protected virtual void Patrol()
+        {
+
+        }
     }
 }
